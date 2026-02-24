79bbc6038a51e38c1d6747427e9e61647035575b
Raphael Gabriel League <rsleague@student.apc.edu.ph>
2026-02-24 05:27:09 +0000

    fix supabase

--- my-profile/frontend/src/supabaseClient.js
+++ my-profile/frontend/src/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+// Change the line below to match your .env file
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
